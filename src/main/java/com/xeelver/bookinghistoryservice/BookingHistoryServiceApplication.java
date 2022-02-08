b934071974928f9a3462de6b8beb9b08992abf26
aleksejs.marmiss <aleksejs.marmiss@gmail.com>
2022-02-08 01:30:42 +0100

    Initial structure created

---
+++ src/main/java/com/xeelver/bookinghistoryservice/BookingHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xeelver.bookinghistoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingHistoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingHistoryServiceApplication.class, args);
+    }
+
+}
